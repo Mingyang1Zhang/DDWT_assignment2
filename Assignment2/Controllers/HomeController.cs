c8104efa0444802134d432b10ced4ab209dd1184
Mingyang1Zhang <143945856+Mingyang1Zhang@users.noreply.github.com>
2023-09-14 00:41:44 +1000

    UPLOAD ORIGIN

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Food Deliver Report";
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            ViewBag.Title = "Deliver Details";
+            return View();
+        }
+
+        public ActionResult OrderHubs()
+        {
+            ViewBag.Title = "Hub Order Report";
+            return View();
+        }
+
+        public ActionResult Payment()
+        {
+            ViewBag.Title = "Payment Analysis";
+            return View();
+        }
+    }
+}
